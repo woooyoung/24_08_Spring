e19d1863d3f2fab8367873920c7b7fb88980eb25
woooyoung <axdsw121@gmail.com>
2024-08-13 13:18:57 +0900

    JSP 적용

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -2,11 +2,17 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.example.demo.vo.Article;
 
 @Controller
 public class UsrHomeController {
 
+	@RequestMapping("/usr/home/main")
+	public String showMain() {
+		return "/usr/home/main";
+	}
+
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
+	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/usr/home/main.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>MAIN</title>
+</head>
+<body>
+	<h1>MAIN</h1>
+	adsfgasdfgas
+	
+	dfgdsfag
+</body>
+</html>
