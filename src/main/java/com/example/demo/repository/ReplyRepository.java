e949f9e4845c74b0ab1a6e8a1f3948f4bfe42a85
woooyoung <axdsw121@gmail.com>
2024-08-27 14:31:05 +0900

    댓글 작성 폼, 기능 구현

--- src/main/java/com/example/demo/repository/ReplyRepository.java
+++ src/main/java/com/example/demo/repository/ReplyRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -20,4 +21,18 @@ public interface ReplyRepository {
 			ORDER BY R.id ASC;
 				""")
 	public List<Reply> getForPrintReplies(String relTypeCode, int relId);
+
+	@Insert("""
+			INSERT INTO reply
+			SET regDate = NOW(),
+			updateDate = NOW(),
+			memberId = #{loginedMemberId},
+			relTypeCode = #{relTypeCode},
+			relId = #{relId},
+			`body` = #{body}
+			""")
+	public void writeReply(int loginedMemberId, String body, String relTypeCode, int relId);
+
+	@Select("SELECT LAST_INSERT_ID();")
+	public int getLastInsertId();
 }
